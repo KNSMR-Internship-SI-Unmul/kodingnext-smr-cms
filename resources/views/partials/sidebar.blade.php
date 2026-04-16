8ad2780d28c38e76a04ab34fc4eb95dff7922031
Diva Tri Andini <diva.trni@gmail.com>
2026-04-16 18:07:29 +0800

    feat: update hyperlink for page dashboard

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -8,7 +8,7 @@
 
     <nav class="flex-1 px-4 space-y-1.5 overflow-y-auto pb-4 custom-scrollbar">
         
-        <a href="#" class="flex items-center gap-3 px-4 py-2.5 text-sm font-semibold text-brand-blue bg-brand-light-blue-hover rounded-full transition-all">
+        <a href="/dashboard" class="flex items-center gap-3 px-4 py-2.5 text-sm font-semibold text-brand-blue bg-brand-light-blue-hover rounded-full transition-all">
             <svg class="w-5 h-5 text-brand-blue" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
             Dashboard
         </a>

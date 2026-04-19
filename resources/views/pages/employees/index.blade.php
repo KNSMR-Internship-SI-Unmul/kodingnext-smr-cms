0fb39503a27a4033d53bd4dac6564fc607e2afba
Diva Tri Andini <diva.trni@gmail.com>
2026-04-19 14:25:49 +0800

    feat: finish feature

--- resources/views/pages/employees/index.blade.php
+++ resources/views/pages/employees/index.blade.php
@@ -17,7 +17,8 @@
             <span class="text-brand-pink">9</span> Employee
         </h1>
         <div class="flex gap-4">
-            <a href="/employees/roles" class="px-6 py-2.5 h-[42px] bg-brand-white hover:bg-brand-pink text-brand-pink hover:text-white border border-brand-pink font-semibold rounded-lg transition text-sm flex items-center justify-center">
+            <a href="/employees/roles" class="px-6 py-2.5 h-[42px] gap-2 bg-brand-white hover:bg-brand-pink text-brand-pink hover:text-white border border-brand-pink font-semibold rounded-lg transition text-sm flex items-center justify-center">
+                <svg class="w-5 h-5" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z"></path></svg>
                 Manage Roles
             </a>
             

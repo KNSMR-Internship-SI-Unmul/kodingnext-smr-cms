d4b1b08531be9749d8b3da6f062a0528d5e56aa4
Diva Tri Andini <diva.trni@gmail.com>
2026-04-30 18:23:25 +0800

    chore(seeder): update promotion seed data

--- database/seeders/PromotionSeeder.php
+++ database/seeders/PromotionSeeder.php
@@ -19,14 +19,14 @@ class PromotionSeeder extends Seeder
                 'start_date' => '2026-02-23',
                 'end_date' => '2026-03-28',
                 'description' => 'Jika sudah lama ingin mengenalkan coding kepada anak, ini bisa jadi waktu yang tepat. Belajar coding bukan hanya tentang mengikuti perkembangan teknologi, tetapi juga melatih cara berpikir, kreativitas, dan problem solving yang akan bermanfaat dalam jangka panjang. Di momen spesial ini, kami juga menyiapkan kejutan menarik yang bisa parents pilih langsung saat mendaftar 😉 Karena belajar untuk masa depan juga bisa dibuat menyenangkan dan penuh semangat. Promo ini berlaku terbatas selama periode Ramadan, mulai 23 Februari hingga 28 Maret 2026. Jangan sampai terlewat ya!',
-                'image' => 'promotions/ovYXOlUMOOA5fhFWHg0zI44IjKymoSaqVihZxe4e.jpg',
+                'image' => 'promotions/koSwh8CdGDFB7z2dDdWUx4STIXdCCQycrrDxzZBC.jpg',
                 'user_id' => 1,
             ], [
                 'title' => 'Pick Your Lucky Angpao',
                 'start_date' => '2026-02-13',
                 'end_date' => '2026-02-22',
                 'description' => 'Chinese New Year Promo – Pick Your Lucky Angpao! Celebrate the Lunar New Year with a fresh start for your child’s digital journey. Through this special promo, kids can enjoy a fun and meaningful learning experience while exploring coding at Koding Next Samarinda. Pick Your Lucky Angpao and get a registration discount of 500,000 for coding programs Available from February 13 to 22, 2026, this is the perfect opportunity to begin a creative and future-ready learning adventure. Don’t miss out.',
-                'image' => 'promotions/IxrBZXAi6zAIPhYF9GhKPk2tdifHCnwQDLYLoEtX.jpg',
+                'image' => 'promotions/589loNMVusVx8naHPue6nclWiquJkyl3qsYg4s9l.jpg',
                 'user_id' => 2,
             ]
         ];

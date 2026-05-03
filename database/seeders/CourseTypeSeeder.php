55591f4584cf9b90613ed77e2dc28ceb162a7617
Diva Tri Andini <diva.trni@gmail.com>
2026-05-03 17:57:37 +0800

    chore(seeder): update seeder for courses and modules

--- database/seeders/CourseTypeSeeder.php
+++ database/seeders/CourseTypeSeeder.php
@@ -15,12 +15,12 @@ class CourseTypeSeeder extends Seeder
     {
         $courseTypes = [
             [
-                'name' => 'Little Koder',
+                'name' => 'Little Koders',
                 'description' => 'Kursus coding ini dirancang untuk mengajarkan siswa sejak usia 4-8 tentang pemrograman dan meningkatkan pemikiran logis serta keterampilan matematika mereka.',
                 'image' => 'courses/2rX0rVIdcRQ3GazNQYoA1D94jSDR0teqY7AjkeK0.jpg',
                 'user_id' => 1,
             ], [
-                'name' => 'Junior Koder',
+                'name' => 'Junior Koders',
                 'description' => 'Kursus coding ini diperuntukkan bagi siswa berusia 8 hingga 16 tahun. Program ini menawarkan kursus pemula dalam pemrograman blok, seperti Game 2D dan Pengembangan Aplikasi Mobile, dan kursus lanjutan dalam pemrograman berbasis teks, seperti Python, JavaScript, dan Smart Home IoT.',
                 'image' => 'courses/zcUvNeEFMjBeWMFN9PjWzqPjAQ8LK4BnmsrJR8NL.jpg',
                 'user_id' => 1,

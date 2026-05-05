b0422233edcc1429877e6bbaae6842c5d71c4a8e
Diva Tri Andini <diva.trni@gmail.com>
2026-05-05 17:40:39 +0800

    feat(frontend): install Alpine.js and initialize in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
 import './bootstrap';
+import Alpine from 'alpinejs';
+
+import { roleManager, employeeManager, promotionManager, eventManager, courseTypeManager, moduleManager, studentManager,  } from './components.js';
+
+window.Alpine = Alpine;
+
+Alpine.data('roleManager', roleManager);
+Alpine.data('employeeManager', employeeManager);
+Alpine.data('promotionManager', promotionManager);
+Alpine.data('eventManager', eventManager);
+// Alpine.data('generalTestimonialManager', generalTestimonialManager);
+Alpine.data('courseTypeManager', courseTypeManager);
+Alpine.data('moduleManager', moduleManager);
+Alpine.data('studentManager', studentManager);
+// Alpine.data('studentProjectManager', studentProjectManager);
+
+
+Alpine.start();

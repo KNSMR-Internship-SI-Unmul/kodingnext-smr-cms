05665fd19caa3f7cc0a6b44bdfd48a0f4822a438
Diva Tri Andini <diva.trni@gmail.com>
2026-05-09 01:32:04 +0800

    style(toast): add new toast notification for error and change delete colors

--- resources/views/components/toast.blade.php
+++ resources/views/components/toast.blade.php
@@ -17,11 +17,18 @@
             'icon'    => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M5 13l4 4L19 7"></path>',
         ],
         'delete' => [
+            'wrapper' => 'bg-[#E8F6ED]',
+            'text'    => 'text-[#16A34A]',
+            'border'  => 'border-[#16A34A]',
+            'icon_bg' => 'bg-[#B7E2C7] text-[#16A34A]',
+            'icon'    => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>',
+        ],
+        'error' => [
             'wrapper' => 'bg-[#FEF2F2]',
             'text'    => 'text-[#FF383C]',
             'border'  => 'border-[#FF383C]',
             'icon_bg' => 'bg-[#FEE2E2] text-[#FF383C]',
-            'icon'    => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>',
+            'icon'    => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z"></path>',
         ],
     ];
 

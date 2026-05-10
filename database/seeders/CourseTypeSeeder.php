9fea4924bbaeecc13702db70af6ac8a5293ba08b
Diva Tri Andini <diva.trni@gmail.com>
2026-05-10 17:08:13 +0800

    chore(seeder): update description

--- database/seeders/CourseTypeSeeder.php
+++ database/seeders/CourseTypeSeeder.php
@@ -16,19 +16,19 @@ class CourseTypeSeeder extends Seeder
         $courseTypes = [
             [
                 'name' => 'Little Koders',
-                'description' => 'Kursus coding ini dirancang untuk mengajarkan siswa sejak usia 4-8 tentang pemrograman dan meningkatkan pemikiran logis serta keterampilan matematika mereka.',
+                'description' => 'Kursus Coding ini dirancang untuk membantu siswa-siswi mengenal dunia pemrograman sejak dini dengan cara yang seru, interaktif, dan mudah dipahami. Melalui pembelajaran berbasis praktik langsung, peserta dapat mengembangkan kemampuan berpikir logis, kreativitas, serta keterampilan pemecahan masalah dan matematika. Didukung oleh pengajar berpengalaman dan metode belajar yang menarik, setiap siswa akan mendapatkan pengalaman belajar yang menyenangkan sekaligus bermanfaat untuk masa depan mereka.',
                 'image' => 'courses/2rX0rVIdcRQ3GazNQYoA1D94jSDR0teqY7AjkeK0.jpg',
                 'user_id' => 1,
             ], 
             [
                 'name' => 'Junior Koders',
-                'description' => 'Kursus coding ini diperuntukkan bagi siswa berusia 8 hingga 16 tahun. Program ini menawarkan kursus pemula dalam pemrograman blok, seperti Game 2D dan Pengembangan Aplikasi Mobile, dan kursus lanjutan dalam pemrograman berbasis teks, seperti Python, JavaScript, dan Smart Home IoT.',
+                'description' => 'Program ini dirancang khusus untuk siswa usia 8 hingga 16 tahun yang ingin mulai mengenal dunia teknologi dan pemrograman. Peserta akan belajar dari tingkat dasar menggunakan pemrograman berbasis blok untuk membuat game 2D dan aplikasi mobile sederhana, hingga tingkat lanjutan menggunakan bahasa pemrograman populer seperti Python dan JavaScript serta teknologi Smart Home IoT. Dengan materi yang mengikuti perkembangan tren digital terbaru, program ini membantu siswa mengembangkan keterampilan teknologi, kreativitas, dan kemampuan problem solving yang relevan untuk masa depan.',
                 'image' => 'courses/zcUvNeEFMjBeWMFN9PjWzqPjAQ8LK4BnmsrJR8NL.jpg',
                 'user_id' => 1,
             ], 
             [
                 'name' => 'RoboNext',
-                'description' => 'Kursus robotika dari Koding Next ini dirancang untuk memperkenalkan teknologi melalui pengalaman langsung. Siswa tidak hanya belajar coding dan merakit robot, tetapi juga memahami cara teknologi bekerja untuk menyelesaikan masalah dunia nyata.',
+                'description' => 'Program ini dirancang untuk mengenalkan dunia robotika dan teknologi kepada anak-anak melalui pembelajaran yang interaktif dan praktik langsung. Peserta akan belajar coding, merakit robot, serta memahami cara kerja teknologi untuk menciptakan solusi dari berbagai permasalahan di kehidupan sehari-hari. Dengan metode belajar yang seru dan kreatif, program ini membantu mengembangkan kemampuan berpikir logis, inovasi, dan keterampilan teknologi sejak dini.',
                 'image' => 'courses/NPBXnFHquexMRfHNRlNNEvODhjSTUMsOHuV2kySp.jpg',
                 'user_id' => 1,
             ]
